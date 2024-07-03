79f786f616d5cf8881a8e6aae505f38aeb21c8f6
Amit singh <ammi431@gmail.com>
2024-07-03 11:57:06 +0530

    Initial Commit

---
+++ InstPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstPost
+//
+//  Created by Amit singh on 03/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
